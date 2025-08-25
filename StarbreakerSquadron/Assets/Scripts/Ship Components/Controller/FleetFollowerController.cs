d7d6ae2179a6e341a86d000da3a83548623ae21f
greatbrick <speharley@gmail.com>
2025-08-25 12:24:25 -0400

    Continued Work On Fleet Spawner

---
+++ StarbreakerSquadron/Assets/Scripts/Ship Components/Controller/FleetFollowerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections.Generic;
+
+public class FleetFollowerController : NetworkBehaviour
+{
+    private Vector2 inputVec = Vector2.zero;
+    private byte inputActives = 0;
+
+    private Transform waypoint;
+    private Vector3 waypointOffset = Vector3.zero;
+
+    [SerializeField]
+    float waypointCarefulRadius = 1.0f;
+    [SerializeField]
+    float carefulSpeed = 2.0f;
+
+    private Movement movement;
+    private WeaponsHolder weaponsHolder;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+        weaponsHolder = GetComponent<WeaponsHolder>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!IsServer) return;
+
+        inputVec = Vector2.zero;
+        inputActives = 0b0000;
+
+        MoveToLocation(waypoint.position + waypointOffset);
+
+        movement.inputVector = inputVec;
+        weaponsHolder.inputActives = inputActives;
+    }
+
+    private Vector2 MoveToLocation(Vector2 location, float maxSpeed = float.MaxValue)
+    {
+        Vector2 output;
+        output.x = movement.RecommendTurnDirection(location);
+        if (rb.linearVelocity.magnitude > maxSpeed) output.y = 0;
+        else output.y = Vector2.Dot(transform.up, (location - transform.position.FlattenVec3()).normalized);
+        Debug.DrawLine(transform.position, location, Color.yellow);
+        return output;
+    }
+}
